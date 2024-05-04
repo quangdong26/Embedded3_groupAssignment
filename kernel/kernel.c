2884b627c2653c41bb44cede10bf2f5de63dc10a
Huynh Quang Dong <huynhquangdong2632003@gmail.com>
2024-05-04 14:00:12 +0700

    add delay.c, try to make a small video with 2 frames (type 'v' to play the video)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,14 +3,21 @@
 #include "../uart/uart1.h"
 #include "framebf.h"
 #include "image.h"
+#include "video.h"
+#include "delay.h"
 
 // #define MAX_CMD_SIZE 100
 #define IMAGE_WIDTH 732 
-#define IMAGE_HEIGHT 587
+#define IMAGE_HEIGHT 578
+#define VIDEO_WIDTH 1280
+#define VIDEO_HEIGHT 720
 
 int currentY = 0;
 int currentX = 0;
 int pastY = 0;
+// Assuming fd is the file descriptor of the opened file
+// buf is the buffer where the data will be stored
+// count is the number of bytes to read
 
 void cli () {
 	// static char cli_buffer[MAX_CMD_SIZE];
@@ -37,6 +44,15 @@ void cli () {
         currentY += 100; // scroll down by 10 pixels
         deleteImage(currentX, pastY, IMAGE_WIDTH,  IMAGE_HEIGHT);
         displayImage(currentX, currentY, image, IMAGE_WIDTH,  IMAGE_HEIGHT);
+    } else if (c == 'v') {
+        deleteImage(currentX, currentY, IMAGE_WIDTH,  IMAGE_HEIGHT);
+        displayImage(0, 0, f001 , VIDEO_WIDTH,  VIDEO_HEIGHT);
+        for (int i =1; i<2; i++){
+            unsigned long frame = frameArray[i];
+            wait_msec(200000);
+            deleteImage(0, 0, VIDEO_WIDTH,  VIDEO_HEIGHT);
+            displayImage(0, 0, frame , VIDEO_WIDTH,  VIDEO_HEIGHT);
+        }
     }
 
 
@@ -53,16 +69,17 @@ void main()
     framebf_init();
 
     // Draw something on the screen
-    drawRectARGB32(100,100,400,400,0x00AA0000,1); //RED
-    drawRectARGB32(150,150,400,400,0x0000BB00,1); //GREEN
-    drawRectARGB32(200,200,400,400,0x000000CC,1); //BLUE
-    drawRectARGB32(250,250,400,400,0x00FFFF00,1); //YELLOW
-    drawPixelARGB32(300, 300, 0x00FF0000); //RED
+    // drawRectARGB32(100,100,400,400,0x00AA0000,1); //RED
+    // drawRectARGB32(150,150,400,400,0x0000BB00,1); //GREEN
+    // drawRectARGB32(200,200,400,400,0x000000CC,1); //BLUE
+    // drawRectARGB32(250,250,400,400,0x00FFFF00,1); //YELLOW
+    // drawPixelARGB32(300, 300, 0x00FF0000); //RED
     
     displayImage(currentX, currentY, image , IMAGE_WIDTH,  IMAGE_HEIGHT); //starting position (x,y), image, width, height
 
     // echo everything back
     while(1) {
+
         // //read each char
         // char c = uart_getc();
         // //send back
---
+++ kernel/video.h
@@ -0,0 +1,4 @@
+extern const unsigned long frameArray [];
+
+extern const unsigned long f001 [];
+extern const unsigned long f054 [];
